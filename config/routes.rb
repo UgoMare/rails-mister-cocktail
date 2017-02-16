98081f49a62846484b2db8482509087f4da0d113
UgoMare <ugo.mare.epitech@gmail.com>
2017-02-16 18:05:03 +0100

    Add model Cocktail

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # root => racine de l'app
+  # GET /cocktails => index
+  # GET /cocktails/:id => show
+  # GET /cocktails/new => new
+  # POST /cocktails => create
 end
