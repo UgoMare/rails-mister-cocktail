2fa48aa723f76dc8d8eefbd7522f064a091cb710
UgoMare <ugo.mare.epitech@gmail.com>
2017-02-16 19:13:57 +0100

    Dose controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
   # GET /cocktails/new => new
   # POST /cocktails => create
   root 'cocktails#index'
-  resources :cocktails, only: [:index, :show, :new, :create]
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+  end
+  resources :doses, only: [:destroy]
+  # GET /cocktails/:cocktail_id/doses/new
+  # POST /cocktails/:cocktail_id/doses
+  # DELETE /doses/:id
 end
