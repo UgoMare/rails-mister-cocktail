41f9bcdfd2be4587081433dbcd406ec324d024f1
UgoMare <ugo.mare.epitech@gmail.com>
2017-02-16 18:36:27 +0100

    Add model Ingredient + some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,15 @@
+require 'open-uri'
+require 'json'
+
 Cocktail.destroy_all
+Ingredient.destroy_all
 
 cocktail = Cocktail.create!(name: 'Bloody Mary')
+
+ingredients_url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+doc = open(ingredients_url).read
+json = JSON.parse(doc)
+
+json['drinks'].each do |ingredient|
+  Ingredient.create! name: ingredient["strIngredient1"]
+end
