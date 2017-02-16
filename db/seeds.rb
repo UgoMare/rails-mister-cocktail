474436c0bd1c9a1ad2f48000e14b13cb26a6a724
UgoMare <ugo.mare.epitech@gmail.com>
2017-02-16 18:52:34 +0100

    Add model Dose + data

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,8 @@ require 'json'
 Cocktail.destroy_all
 Ingredient.destroy_all
 
-cocktail = Cocktail.create!(name: 'Bloody Mary')
+bloody = Cocktail.create!(name: 'Bloody Mary')
+mojito = Cocktail.create!(name: 'Mojito')
 
 ingredients_url = "http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
 doc = open(ingredients_url).read
@@ -12,4 +13,12 @@ json = JSON.parse(doc)
 
 json['drinks'].each do |ingredient|
   Ingredient.create! name: ingredient["strIngredient1"]
+end
+
+
+descriptions = ['3cl', '10gr', '3 cubes', '5 leaves']
+
+4.times do
+  bloody.doses.create(description: descriptions.sample, ingredient: Ingredient.all.sample) #cocktail_id is already added
+  mojito.doses.create(description: descriptions.sample, ingredient: Ingredient.all.sample) #cocktail_id is already added
 end
